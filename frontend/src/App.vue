abb3d61b73d76feb2de53288033e8a3c4e40ed5a
Spooder <yoshi7591@naver.com>
2024-01-11 23:00:23 +0900

    Navbar 생성 초기 작업(1)

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,30 +1,23 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
+  <nav class="bg-gray-800">
+    <div class="relative flex items-center space-around pt-2 pb-2">
+      <p class="text-white px-3 py-2">위시리스트</p>
+      <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">메인</p>
+      <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">소개</p>
+      <p class="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium">상품 찾기</p>
+    </div>
   </nav>
   <router-view/>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script lang="ts">
+import './index.css';
+import { defineComponent } from 'vue';
 
-nav {
-  padding: 30px;
-}
+export default defineComponent({
+  name: 'HomeView',
+  components: {
 
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+  },
+});
+</script>
--- frontend/src/views/HomeView.vue
+++ frontend/src/views/HomeView.vue
@@ -1,18 +1,17 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js + TypeScript App"/>
+    <p>Home Content</p>
   </div>
 </template>
 
 <script lang="ts">
+import '../index.css';
 import { defineComponent } from 'vue';
-import HelloWorld from '@/components/HelloWorld.vue'; // @ is an alias to /src
 
 export default defineComponent({
   name: 'HomeView',
   components: {
-    HelloWorld,
+
   },
 });
 </script>
