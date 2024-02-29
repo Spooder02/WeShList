68163ad1df09b7a49f0f5525ab55e33ddd78e129
Spooder <yoshi7591@naver.com>
2024-03-01 00:07:46 +0900

    서비스 소개 페이지 제작

--- frontend/src/views/AboutView.vue
+++ frontend/src/views/AboutView.vue
@@ -11,7 +11,21 @@
     </div>
     <p class="text-center font-bold text-2xl mt-8">사용 방법</p>
     <hr class="w-48 h-1 my-2 mx-auto bg-gray-200 border-0 rounded">
-    <div class="w-4/5 mx-auto">
+    <div class="w-4/5 mx-auto my-6">
+      <div class="block">
+        <img src="@/assets/weshlist_howtouse(1).png">
+        <div class="block">
+          <p class="font-semibold text-md">1. 메인 페이지 - 햄버거 박스를 이용해 다른 영역으로 이동이 가능합니다.</p>
+          <p class="text-sm">로그인 영역 - 회원가입 및 로그인을 할 수 있습니다.</p>
+          <p class="text-sm">메인 - 메인 페이지로 이동할 수 있습니다</p>
+          <p class="text-sm">소개 - 서비스 소개 및 사용 방법을 확인할 수 있습니다.</p>
+          <p class="text-sm">상품 찾기 - 사용자들이 등록한 상품들을 확인할 수 있습니다.</p>
+          <p class="text-sm">상품 제보 - 직접 사용자가 상품에 대한 정보를 게시할 수 있습니다.</p>
+        </div>
+        <img class="mt-6" src="@/assets/weshlist_howtouse(2).png">
+        <p class="font-semibold text-md">2. 상품 등록 페이지 - 첨부된 사진을 참고하여 상품을 등록합니다.</p>
+        <p class="text-sm">상품 수정 페이지도 동일한 UI를 사용하며, 상품 등록과 마찬가지로 수정할 수 있습니다.</p>
+      </div>
       
     </div>
   </div>
